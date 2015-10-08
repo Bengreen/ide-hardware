a76e223dc8cbaf36f1850b48eb9bc116274d71ed
Jean-Claude Wippler <jc@wippler.nl>
2015-10-08 23:59:09 +0200

    minimal header for blink demo

--- arm/cores/lpc8xx/Arduino.h
+++ arm/cores/lpc8xx/Arduino.h
@@ -1,4 +1,14 @@
 #ifndef Arduino_h
 #define Arduino_h
 
+#define INPUT 0
+#define OUTPUT 1
+
+#define LOW 0
+#define HIGH 1
+
+extern void pinMode (int pin, int mode);
+extern void digitalWrite (int pin, int level);
+extern void delay (int ms);
+
 #endif
