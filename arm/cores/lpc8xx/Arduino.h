ce9793c71cadcbbc333430f86e03b5b145db5511
Jean-Claude Wippler <jc@wippler.nl>
2015-10-20 15:53:43 +0200

    add basic library primitives

--- arm/cores/lpc8xx/Arduino.h
+++ arm/cores/lpc8xx/Arduino.h
@@ -7,8 +7,13 @@
 #define LOW 0
 #define HIGH 1
 
-extern void pinMode (int pin, int mode);
-extern void digitalWrite (int pin, int level);
-extern void delay (int ms);
+extern "C" void setup ();
+extern "C" void loop ();
+
+extern "C" void pinMode (int pin, int mode);
+extern "C" int digitalRead (int pin);
+extern "C" void digitalWrite (int pin, int level);
+extern "C" void delay (int ms);
+extern "C" unsigned millis ();
 
 #endif
--- arm/cores/lpc8xx/main.cpp
+++ arm/cores/lpc8xx/main.cpp
@@ -1,9 +1,41 @@
-extern void setup ();
-extern void loop ();
+#include "LPC8xx.h"
+#include "Arduino.h"
+
+volatile unsigned msCounter;
+
+extern "C" void SysTick_Handler () {
+    ++msCounter;
+}
 
 int main () {
-  setup();
-  while (true)
-    loop();
-  return 0;
+    SysTick_Config(12000);
+    setup();
+    while (true)
+        loop();
+    return 0;
+}
+
+void pinMode (int pin, int mode) {
+    if (mode == OUTPUT)
+        LPC_GPIO_PORT->DIR0 |= 1<<pin;
+    else
+        LPC_GPIO_PORT->DIR0 &= ~(1<<pin);
+}
+
+int digitalRead (int pin) {
+    return LPC_GPIO_PORT->B0[pin];
+}
+
+void digitalWrite (int pin, int level) {
+    LPC_GPIO_PORT->B0[pin] = level != 0;
+}
+
+unsigned millis () {
+    return msCounter;
+}
+
+void delay (int ms) {
+    unsigned start = msCounter;
+    while (msCounter - start < ms)
+        __WFI();
 }
