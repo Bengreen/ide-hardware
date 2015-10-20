8f15fa5e3c888ebf44d5294a16ed737333146b03
Jean-Claude Wippler <jc@wippler.nl>
2015-10-20 17:01:44 +0200

    cleanup

--- arm/cores/lpc8xx/Arduino.h
+++ arm/cores/lpc8xx/Arduino.h
@@ -7,13 +7,13 @@ const int OUTPUT = 1;
 const int LOW = 0;
 const int HIGH = 1;
 
-extern "C" void setup ();
-extern "C" void loop ();
+extern void setup ();
+extern void loop ();
 
-extern "C" void pinMode (int pin, int mode);
-extern "C" int digitalRead (int pin);
-extern "C" void digitalWrite (int pin, int level);
-extern "C" void delay (int ms);
-extern "C" unsigned millis ();
+extern void pinMode (int pin, int mode);
+extern int digitalRead (int pin);
+extern void digitalWrite (int pin, int level);
+extern void delay (int ms);
+extern unsigned millis ();
 
 #endif
--- arm/cores/lpc8xx/main.cpp
+++ arm/cores/lpc8xx/main.cpp
@@ -1,5 +1,5 @@
-#include "LPC8xx.h"
 #include "Arduino.h"
+#include "LPC8xx.h"
 
 volatile unsigned msCounter;
 
